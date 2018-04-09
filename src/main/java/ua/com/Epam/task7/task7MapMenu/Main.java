9d0f5ffedd63932723cf87f85fffa315af927515
Reveta <gfdsre1999@gmail.com>
2018-04-09 22:12:01 +0300

    task 7 at proses

--- src/main/java/ua/com/Epam/task7/task7MapMenu/Main.java
+++ src/main/java/ua/com/Epam/task7/task7MapMenu/Main.java
@@ -1,5 +1,22 @@
 package ua.com.Epam.task7.task7MapMenu;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
+  public static void main(String[] args) {
+    RedBlackTree<Integer> integers = new RedBlackTree<>();
+    integers.add(12);
+    integers.add(42);
+    integers.add(62);
+    integers.add(122);
+    integers.add(12);
+
+
+    for (int num: integers) {
+      System.out.println(num);
+    }
+  }
 }
